20101c392d986a9cd403a070b1de77b94c056b8a
bsidemyself <shackletron@gmail.com>
2023-05-26 11:14:49 -0700

    added first mate seed and changed models

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,7 @@
 const Captain  = require('./Captain');
 const FirstMate = require('./FirstMate');
 const Powers = require('./Powers');
-const User = require('./user');
+const User = require('./User');
+const CrewChoice = require('./CrewChoice');
 
-module.exports = { Captain, FirstMate, User, Powers };
-function newFunction() {
-    return './User';
-}
-
+module.exports = { Captain, FirstMate, User, Powers, CrewChoice };
